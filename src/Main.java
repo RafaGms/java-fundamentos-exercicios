3ce1379baa7a2a085b6250ff861692b1ac560714
rafael <faelgomes2003@gmail.com>
2026-09-15 22:40:32 -0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        String product1 = "computer";
+        String product2 = "0ffice desk";
+        int age = 21;
+        int code = 344234;
+        char gender = 'F';
+
+        double price1 = 3.2342;
+
+        double price2 = 5.234;
+        double measure = 43.234234;
+
+
+        System.out.println("Products:");
+        System.out.printf("%s, which price is $ %f \n", product1, price1);
+        System.out.printf("%s, which price is $ %f \n", product2, price2);
+        System.out.println("");
+        System.out.printf("Record: %s years old, code %d abd gander: %s %n", age, code, gender);
+    }
+
+}
